0882fad8e54961081320dffad0c6ab8c241ba6b0
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-04-09 02:42:54 -0500

    movie details.jsx

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,31 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import MovieList from '../MovieList/MovieList'
+import "./App.css";
+import AddMovie from "../AddMovie/AddMovie";
+import MovieList from "../MovieList/MovieList";
+import Details from "../Details/Details";
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
+        <h1>The Movies Saga!</h1>
+
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         {/* Details page */}
+        <Route path="/details/:id" exact>
+          <Details />
+        </Route>
 
         {/* Add Movie page */}
+        <Route path="/addMovie" exact>
+          <AddMovie />
+        </Route>
       </Router>
     </div>
   );
 }
-
+g;
 
 export default App;
