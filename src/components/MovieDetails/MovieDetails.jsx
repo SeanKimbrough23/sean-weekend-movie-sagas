ea366a2aa6673cd4c0355ddcde5bb7ffaf60d462
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-04-09 02:19:40 -0500

    details.jsx component

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,39 @@
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function MovieDetails() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const genres = useSelector((store) => store.genres);
+  const movie = useSelector((store) => store.movie);
+  const { id } = useParams();
+
+  const handleBack = () => {
+    history.goBack();
+  };
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_GENRES", payload: id });
+    dispatch({ type: "FETCH_MOVIE", payload: id });
+  }, []);
+
+  return (
+    <>
+      <div className="movieItem">
+        <h2>{movie.title}</h2>
+        <img src={movie.poster} alt={movie.title} />
+      </div>
+      <h4>{genres}</h4>
+      <h4 className="description">{movie.description}</h4>
+      <button className="back" onClick={handleBack}>
+        Back To List
+      </button>
+    </>
+  );
+}
+
+export default MovieDetails;
